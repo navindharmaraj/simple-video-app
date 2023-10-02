8211a07ab0837ab8a19b301b62482ccbb371b102
Navin Dharmarajan <navindharmaraj@users.noreply.github.com>
2023-10-02 16:31:53 +1100

    Initial commit

--- index.js
+++ index.js
@@ -13,5 +13,13 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Health Check Endpoint
+app.get('/health', (req, res) => {
+    // You can perform any checks here to verify the health of your application
+    // For example, checking the status of your database connection.
+    // If everything is ok, send a status code 200
+    res.status(200).send('OK');
+  });
+
 // Listen on port 3000
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
